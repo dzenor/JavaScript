bd169bf9c8145e91e11428ea954b79273a91b0c4
dzenor <dzenisredzovic12@gmail.com>
2022-11-22 20:48:39 +0100

    ex2.2

---
+++ JavaScript/01_GettingStartedWithJavaScript/Ex2.2/main.js
@@ -0,0 +1,13 @@
+let name = "Dzeno";
+let age = "20";
+let canCode = "true";
+
+console.log(
+  "Hi my name is " +
+    name +
+    " I'm " +
+    age +
+    " years old and i can code JavaScript: " +
+    canCode +
+    "."
+);
