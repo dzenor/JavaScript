27f6ea6faa10fe0164c55626d4e647807b8c9f23
dzenor <dzenisredzovic12@gmail.com>
2022-11-24 19:30:06 +0100

    zadaca 2.4

---
+++ JavaScript/01_GettingStartedWithJavaScript/Ex2.4/main.js
@@ -0,0 +1,17 @@
+alert("Exercise 2.4");
+
+let a = 100;
+console.log(a);
+let b = 3;
+console.log(b);
+let c = 20;
+console.log(c);
+
+a = a + b;
+console.log(a);
+
+a = a / c;
+console.log(a);
+
+c = c % b;
+console.log(c);
