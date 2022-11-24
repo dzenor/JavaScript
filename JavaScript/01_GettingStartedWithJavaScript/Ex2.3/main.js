95a6fe3e2ebd39787bc85f79328b449b9be15253
dzenor <dzenisredzovic12@gmail.com>
2022-11-24 18:57:18 +0100

    zadaca 2.3

---
+++ JavaScript/01_GettingStartedWithJavaScript/Ex2.3/main.js
@@ -0,0 +1,14 @@
+alert("Zdravo!");
+
+let a = prompt("Vnesete ja stranata: 'a'");
+console.log(a);
+
+let b = prompt("Vnesete ja stranata: 'b'");
+console.log(b);
+
+a = a ** 2;
+b = b ** 2;
+
+c = a + b;
+c = Math.sqrt(c);
+console.log(c);
