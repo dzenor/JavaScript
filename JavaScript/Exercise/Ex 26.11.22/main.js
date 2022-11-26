80c9548eb9a58529bfb6cf538744215d5cfe1532
dzenor <dzenisredzovic12@gmail.com>
2022-11-26 19:22:58 +0100

    ex 1

---
+++ JavaScript/Exercise/Ex 26.11.22/main.js
@@ -0,0 +1,49 @@
+let a = 15;
+console.log(typeof 15);
+//prediction number
+
+let b = 5.5;
+console.log(typeof 5.5);
+//prediction number
+
+let c = NaN;
+console.log(typeof NaN);
+
+let d = "Hello";
+console.log(typeof "Hello");
+
+let e = true;
+console.log(typeof true);
+
+let f = 1 != 2;
+console.log(typeof f);
+
+let g = "hamburger";
+console.log(g + "s");
+
+let h = "hamburgers" - "s";
+console.log(h);
+//prediction hamburger
+//actual NaN
+
+let i = "1" + "3";
+console.log(i);
+//prediction 4
+//actual 13
+
+let j = "1" - "3";
+console.log(j);
+//prediction nz
+//actual -2
+
+let k = "johnny" + 5;
+console.log(k);
+//actual johnny5
+
+let l = "johnny" - 5;
+console.log(l);
+//actual NaN
+
+let m = 99 * "luftbaloons";
+console.log(m);
+//actual NaN
