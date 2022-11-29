6544affaf815d81e1d9ddb3a50fc80c8e30a7fce
dzenor <dzenisredzovic12@gmail.com>
2022-11-29 18:23:06 +0100

    exercises

---
+++ JavaScript/Exercise/ex home/ex.1/main.js
@@ -0,0 +1,6 @@
+alert("exercise 1");
+
+let oneKmToMiles = 0.621371;
+
+let miles = oneKmToMiles * 130;
+console.log(`The distance of 130 kms is equal to ${miles}`);
---
+++ JavaScript/Exercise/ex home/ex.2/main.js
@@ -0,0 +1,13 @@
+alert("Exercise 2 homework");
+
+let heightInch = 5;
+let cm = 2.54;
+let inchToCm = heightInch * cm;
+
+console.log(`${heightInch} inch is eaqual to ${inchToCm} cm.`);
+
+let weightPounds = 7;
+let kilo = 0.45359237;
+let poundsToKilo = weightPounds * kilo;
+
+console.log(`${weightPounds} pounds is eaqual to ${poundsToKilo} kilo`);
