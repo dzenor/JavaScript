7703d997c01203e2c541b677bdd9770c16daaa49
dzenor <dzenisredzovic12@gmail.com>
2022-12-01 20:29:08 +0100

    ex.3.5

---
+++ 03_JavaScriptMultipleValues/Ex3.5/main.js
@@ -0,0 +1,27 @@
+let people = {
+  friends: [],
+};
+
+let duki = {
+  ime: "Merih",
+  prezime: "Tafilovic",
+  godine: "22",
+  ID: 1,
+};
+
+let onur = {
+  ime: "Onur",
+  prezime: "Mok",
+  godine: "37",
+  ID: 2,
+};
+
+let mersad = {
+  ime: "Onur",
+  prezime: "Mok",
+  godine: "37",
+  ID: 3,
+};
+
+people.friends.push(duki, onur, mersad);
+console.log(people);
