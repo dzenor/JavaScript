28447163ef4dfde5b027496f6230698fd865da7a
dzenor <dzenisredzovic12@gmail.com>
2022-12-03 18:04:02 +0100

    project 2

---
+++ 03_JavaScriptMultipleValues/Project.2/main.js
@@ -0,0 +1,26 @@
+let inventory = {
+    storeItems = []
+};
+
+let samsung = {
+    model: galaxy8,
+    cost: 370,
+    quantity: ksaj 
+};
+
+let iphone = {
+    model: galaxy8,
+    cost: 370,
+    quantity: ksaj 
+};
+
+let huawei = {
+    model: galaxy8,
+    cost: 370,
+    quantity: ksaj 
+};
+
+storeItems.push( samsung,iphone,huawei );
+console.log(store.storeItems);
+
+
